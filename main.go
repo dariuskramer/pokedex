b517dde13d7fbbf7971ab6b0885594435f15e3b3
Donovan <commit@dkrm.dev>
2025-05-14 18:38:14 +0200

    add map & mapb commands

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -11,8 +13,27 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(config *config) error
 }
+
+type config struct {
+	Next     string
+	Previous string
+}
+
+type LocationAreas struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous string `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+const (
+	PokeApiLocationAreasURL = "https://pokeapi.co/api/v2/location-area/"
+)
 
 var supportedCommands map[string]cliCommand
 
@@ -28,12 +49,22 @@ func init() {
 			description: "Display a help message",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Display the next 20 location areas in the Pokemon world",
+			callback:    commandMap,
+		},
+		"mapb": {
+			name:        "mapb",
+			description: "Display the previous 20 location areas in the Pokemon world",
+			callback:    commandMapb,
+		},
 	}
 }
 
 func main() {
 	const Prompt = "Pokedex > "
-
+	config := &config{Next: PokeApiLocationAreasURL}
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
@@ -52,7 +83,10 @@ func main() {
 			continue
 		}
 
-		command.callback()
+		err := command.callback(config)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
@@ -65,13 +99,13 @@ func cleanInput(text string) []string {
 	return result
 }
 
-func commandExit() error {
+func commandExit(config *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *config) error {
 	usage := `
 Welcome to the Pokedex!
 Usage:
@@ -82,5 +116,59 @@ Usage:
 	}
 
 	fmt.Println(usage)
+	return nil
+}
+
+func internalCommandMap(url string, config *config) error {
+	response, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+	defer response.Body.Close()
+
+	var payload LocationAreas
+	decoder := json.NewDecoder(response.Body)
+	err = decoder.Decode(&payload)
+	if err != nil {
+		return fmt.Errorf("%v", err)
+	}
+
+	for _, location := range payload.Results {
+		fmt.Println(location.Name)
+	}
+
+	config.Next = payload.Next
+	config.Previous = payload.Previous
+
+	return nil
+}
+
+func commandMap(config *config) error {
+	url := config.Next
+
+	if url == "" {
+		return fmt.Errorf("map: no next location areas")
+	}
+
+	err := internalCommandMap(url, config)
+	if err != nil {
+		return fmt.Errorf("map: %v", err)
+	}
+
+	return nil
+}
+
+func commandMapb(config *config) error {
+	url := config.Previous
+
+	if url == "" {
+		return fmt.Errorf("mapb: no previous location areas")
+	}
+
+	err := internalCommandMap(url, config)
+	if err != nil {
+		return fmt.Errorf("mapb: %v", err)
+	}
+
 	return nil
 }
